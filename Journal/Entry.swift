9ee9ccb8c0d5da06efd8d1131b321592a5e6c940
Scott <lordkappos@gmail.com>
2015-10-15 15:57:58 -0600

    Class and StoryBoard

---
+++ Journal/Entry.swift
@@ -0,0 +1,17 @@
+//
+//  Entry.swift
+//  Journal
+//
+//  Created by Scott on 10/15/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class Entry: NSObject {
+    
+    var entryTitle: String?
+    var entryBody: String?
+    var entryTimeStamp: String?
+
+}
---
+++ Journal/EntryController.swift
@@ -0,0 +1,17 @@
+//
+//  EntryController.swift
+//  Journal
+//
+//  Created by Scott on 10/15/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class EntryController: NSObject {
+    
+    //TODO: need help with CRUD
+    
+    
+
+}
